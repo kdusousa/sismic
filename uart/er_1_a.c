5bc62441ae616355225eb05971b82c8f98b9f881
kdusousa <kdusousa@gmail.com>
2021-04-13 14:22:03 -0300

    pasta uart

---
+++ uart/er_1_a.c
@@ -0,0 +1,88 @@
+#include <msp430.h> 
+
+#define TRUE 1
+#define FALSE 0
+void USCI_A0_config(void);
+void led_VM(void);
+void led_vm(void);
+void led_VD(void);
+void led_vd(void);
+void leds_config(void);
+void er_1(void);
+
+volatile char letra_tx='A',letra_rx;
+
+void main (void){
+
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+    leds_config();
+    USCI_A0_config();
+    __enable_interrupt(); //por interrupção
+    //letra_tx='A'; //por polling
+    UCA0TXBUF='A'; //faz a primeira transmissão
+    while(TRUE){
+        //er_1();
+        if (letra_tx == letra_rx){ //Verificar
+                led_VD(); led_vm(); //OK
+        }
+        else{
+                led_vd(); led_VM(); //Erro
+        }
+     }
+
+
+    return 0;
+}
+// Interrupção da USCI_A0
+//#pragma vector = 56
+#pragma vector = USCI_A0_VECTOR
+__interrupt void usci_a0_int(void){
+    volatile static letra='A';
+    UCA0IV; //Apagar RXIFG
+    letra_rx=UCA0RXBUF; //Letra recebida
+    letra_tx=letra; //Letra transmitida
+    if (++letra=='Z'+1) //Próxima letra
+        letra='A'; //Voltar para letra A
+     UCA0TXBUF=letra; //Transmitir
+}
+
+
+
+void er_1(void){
+    //Laço principal
+    while ( (UCA0IFG&UCTXIFG)==0); //Esperar TXIFG=1
+    UCA0TXBUF=letra_tx;
+    while ( (UCA0IFG&UCRXIFG)==0); //Esperar RXIFG=1
+    letra_rx=UCA0RXBUF;
+    if (letra_tx == letra_rx){ //Verificar
+        led_VD(); led_vm(); //OK
+        }
+     else{
+        led_vd(); led_VM(); //Erro
+     }
+     if (++letra_tx == 'Z'+1) letra_tx='A'; //Próxima letra
+}
+
+// Configurar USCI_A0
+void USCI_A0_config(void){
+    UCA0CTL1 = UCSWRST; //RST=1 para USCI_A0
+    UCA0CTL0 = 0; //sem paridade, 8 bits, 1 stop, modo UART
+    UCA0STAT = UCLISTEN; //Loop Back
+    UCA0BRW = 3; // Divisor
+    UCA0MCTL = UCBRS_3; //Modulador = 3 e UCOS=0
+    UCA0CTL1 = UCSSEL_1; //RST=0 e Selecionar ACLK
+    UCA0IE = UCRXIE; //Hab. Interrup recepção
+}
+
+// Controle dos leds
+void led_VM(void){ P1OUT |= BIT0; } //led Vermelho aceso
+void led_vm(void){ P1OUT &= ~BIT0; } //led Vermelho apagado
+void led_VD(void){ P4OUT |= BIT7; } //led Verde aceso
+void led_vd(void){ P4OUT &= ~BIT7; } //led Verde apagado
+
+// Configurar Leds
+void leds_config(void){
+ P1DIR |= BIT0; P1OUT &= ~BIT0; //Led vermelho
+ P4DIR |= BIT7; P4OUT &= ~BIT7; //Led verde
+}
+
